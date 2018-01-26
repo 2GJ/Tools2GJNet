e23e11db8a33d8d5eabb6fb4b13a0817e657adb8
2GJ <GermanGJ@Hotamil.com>
2018-01-26 15:06:56 -0500

    20180126 - 2GJ Inicio git Tools2GJ

---
+++ String2GJ.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tools2GJ
+{
+    public class String2GJ
+    {
+        public String ConvBase64UTF8ToString(String InStr)
+        {
+            return Encoding.UTF8.GetString(Convert.FromBase64String(InStr));
+        }
+
+        public String ConvStringToBase64UTF8(String InStr)
+        {
+            return Convert.ToBase64String(Encoding.UTF8.GetBytes(InStr));
+        }
+    }
+}
